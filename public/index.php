dd83201b82eaec279bce54e62817efa8dd971cb6
Víctor Ponz <victorponz@gmil.com>
2021-11-19 11:43:06 +0100

    Version 1.6

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true
+    ],
+];
+$app = new \Slim\App($config);
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
